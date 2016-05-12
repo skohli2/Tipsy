9341104c75e8bdeba0889502fb9677a536d09947
skohli2 <skohli2@L-SNVT1ZBF1G-M.local>
2016-05-12 00:12:10 -0700

    tipsy

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+        
+    }
+    @IBAction func CalculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
